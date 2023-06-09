93f3963bc255c0306b42392aa1d265ab32d8c33e
Kartik0899 <jaybudhraja08@gmail.com>
2023-06-10 00:09:17 +0530

    Toast update

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,15 +1,20 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { signIn } from './features/auth/authSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { signIn, selectError } from './features/auth/authSlice';
+
 
 const SignIn = () => {
 
     const dispatch = useDispatch();
+    const error = useSelector(selectError);
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [rememberMe, setRememberMe] = useState(true);
 
-    const handleSignIn = (e) => {
+    const handleSignIn = async (e) => {
         e.preventDefault();
 
         const signInPayload = {
@@ -18,7 +23,21 @@ const SignIn = () => {
             rememberMe,
         };
 
-        dispatch(signIn(signInPayload));
+        // dispatch(signIn(signInPayload));
+
+        try {
+            const response = await dispatch(signIn(signInPayload));
+            // Handle successful sign-in
+            // Show success toast or redirect to another page
+            toast.success('Welcome');// Show error toast
+        } catch (error) {
+            // Handle sign-in error
+            // if (error.message === 'Failed to sign in') {
+                toast.error('Wrong credentials. Please try again.');
+            // } else {
+                // toast.error('Sign-in failed. Please try again.');
+            // }
+        }
     };
 
     // const dispatch = useDispatch();
@@ -36,6 +55,7 @@ const SignIn = () => {
 
     return (
         <div>
+
             <section className="bg-gray-50 dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -46,8 +66,8 @@ const SignIn = () => {
                             <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSignIn}>
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                                    placeholder="name@sigmoid.com" required="" />
+                                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        placeholder="name@sigmoid.com" required="" />
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -75,6 +95,11 @@ const SignIn = () => {
                     </div>
                 </div>
             </section>
+
+            <ToastContainer 
+                position="bottom-right"
+                autoClose={2000}
+            />
             {/* <h2>Sign In  email: 'candidate@sigmoid.com',
                 password: 'Sigmoid#123',</h2>
             <form onSubmit={handleSignIn}>
--- src/components/features/auth/authSlice.js
+++ src/components/features/auth/authSlice.js
@@ -33,15 +33,33 @@ const authSlice = createSlice({
 
 export const { signInStart, signInSuccess, signInFailure, signOutSuccess } = authSlice.actions;
 
-export const signIn = (payload) => async (dispatch) => {
+// export const signIn = (payload) => async (dispatch) => {
+//   try {
+//     dispatch(signInStart());
+//     const user = await signInAsync(payload);
+//     dispatch(signInSuccess(user));
+//   } catch (error) {
+//     dispatch(signInFailure(error.message));
+//   }
+// };
+
+export const signIn = (signInPayload) => async (dispatch) => {
   try {
     dispatch(signInStart());
-    const user = await signInAsync(payload);
-    dispatch(signInSuccess(user));
+    const response = await signInAsync(signInPayload);
+    dispatch(signInSuccess(response));
+    // Return the response for further handling if needed
+    return response;
   } catch (error) {
     dispatch(signInFailure(error.message));
+    throw error;
   }
 };
+
+export const selectUser = (state) => state.auth.user;
+export const selectLoading = (state) => state.auth.isLoading;
+export const selectError = (state) => state.auth.error;
+
 
 export const signOut = () => (dispatch) => {
   // Perform sign out logic (e.g., clear user session, remove tokens, etc.)
