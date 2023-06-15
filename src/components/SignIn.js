bd712037c0bdba1509c4af9f4ac5f99fe41542e7
Kartik0899 <72159742+Kartik0899@users.noreply.github.com>
2023-06-16 03:18:38 +0530

    Update SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -30,9 +30,7 @@ const SignIn = () => {
                 toast.error('Wrong credentials. Please try again.');
         }
     };
-    // email: 'candidate@sigmoid.com',
-    // password: 'Sigmoid#123'
-
+   
     return (
         <div>
 
@@ -47,7 +45,7 @@ const SignIn = () => {
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        placeholder="name@sigmoid.com" required="" />
+                                        placeholder="name@gmail.com" required="" />
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
