0def4018ff8fbc5f335e4b8d89ae643c395d07f8
Kartik0899 <72159742+Kartik0899@users.noreply.github.com>
2023-06-18 01:37:56 +0530

    minor Update BarChart.js

--- src/components/Charts/BarChart.js
+++ src/components/Charts/BarChart.js
@@ -13,7 +13,7 @@ import { Bar } from 'react-chartjs-2';
 
 const BarChart = ({ data, options }) => {
     return (
-        <div className='w-[70%] border-r-2 p-3'>
+        <div className='lg:w-[80%] md:w-[100%] p-3'>
             <div className='flex items-center my-[2%] text-white text-xl'>Bar chart -</div>
             <Bar data={data} options={options} />
         </div>
