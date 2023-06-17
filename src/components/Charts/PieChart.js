aaac29b0f53d9032093ab22a68605e4998eeed02
Kartik0899 <72159742+Kartik0899@users.noreply.github.com>
2023-06-18 01:39:17 +0530

    minor Update PieChart.js

--- src/components/Charts/PieChart.js
+++ src/components/Charts/PieChart.js
@@ -13,7 +13,7 @@ import { Pie } from 'react-chartjs-2';
 
 const PieChart = ({ data, options }) => {
     return (
-        <div className='w-[30%] p-3'>
+        <div className='lg:w-[40%] md:w-[70%] p-3'>
             <div className='flex items-center my-[2%] text-white text-xl'>Pie chart -</div>
             <Pie data={data} options={options} />
         </div>
