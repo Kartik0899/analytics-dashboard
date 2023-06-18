e8ec187953d783dfe0200b8c667a99f273181716
Kartik0899 <72159742+Kartik0899@users.noreply.github.com>
2023-06-18 23:54:15 +0530

    Update SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -34,24 +34,24 @@ const SignIn = () => {
     return (
         <div>
 
-            <section className="bg-gray-50 dark:bg-gray-900">
+             <section className="bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md bg-gray-800 border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            <h1 className="text-xl font-bold leading-tight tracking-wide md:text-2xl text-white">
                                 Sign in to your account
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSignIn}>
                                 <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+                                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
                                         placeholder="name@gmail.com" required="" />
                                 </div>
                                 <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
                                     <input type="password"
                                         value={password} onChange={(e) => setPassword(e.target.value)}
-                                        name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                        name="password" id="password" placeholder="••••••••" className="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white" required="" />
                                 </div>
                                 <div className="flex items-center justify-between">
                                     <div className="flex items-start">
@@ -59,15 +59,15 @@ const SignIn = () => {
                                             <input id="remember" aria-describedby="remember" type="checkbox"
                                                 checked={rememberMe}
                                                 onChange={(e) => setRememberMe(e.target.checked)}
-                                                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
+                                                className="w-4 h-4" required="" />
                                         </div>
                                         <div className="ml-3 text-sm">
-                                            <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                            <label htmlFor="remember" className="text-gray-300">Remember me</label>
                                         </div>
                                     </div>
-                                    <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                                    <a href="#" className="text-sm font-medium hover:underline text-primary-500">Forgot password?</a>
                                 </div>
-                                <button type="submit" className="w-full text-white bg-[#2563eb] hover:bg-[#2563eb] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-[#2563eb] dark:hover:bg-[#0b55f4] dark:focus:ring-primary-800">Sign in</button>
+                                <button type="submit" className="w-full text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-[#2563eb] hover:bg-[#0b55f4]">Sign in</button>
                             </form>
                         </div>
                     </div>
