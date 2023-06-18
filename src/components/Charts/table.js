d6f2e34a32751bbd632eec8cebcd4499003e2002
Kartik0899 <72159742+Kartik0899@users.noreply.github.com>
2023-06-18 23:31:47 +0530

    Update table.js

--- src/components/Charts/table.js
+++ src/components/Charts/table.js
@@ -6,9 +6,9 @@ const TableComponent = ({ data }) => {
 
             <div className='flex items-center my-[2%] text-white text-xl'>Table -</div>
             <div className="relative overflow-x-auto h-[20rem]">
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead className=" text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr className='sticky top-0 bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+                <table className="w-full text-sm text-left text-gray-500">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-700">
+                        <tr className='sticky top-0 bg-gray-700 text-gray-400'>
                             <th scope="col" className="px-6 py-3">
                                 Publisher ID
                             </th>
@@ -19,8 +19,8 @@ const TableComponent = ({ data }) => {
                     </thead>
                     <tbody>
                         {data.map((item, index) => (
-                            <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <tr key={index} className="border-b bg-gray-800 border-gray-700">
+                                <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap text-white">
                                     {item.publisherId}
                                 </th>
                                 <td className="px-6 py-4">
